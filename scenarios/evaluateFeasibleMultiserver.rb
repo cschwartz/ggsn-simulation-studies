42fafa2a2c9cd72071c12050bbfd57363929a0af
Florian Metzger <florian.metzger@univie.ac.at>
2013-09-02 15:04:56 +0200

    add multiserver scenario

---
+++ scenarios/evaluateFeasibleMultiserver.rb
@@ -0,0 +1,12 @@
+repetitions 10
+maximumNumberOfTunnels = (10..1000).step(10)
+maximumNumberOfInstances = (10...1000).step(10)
+
+maximumNumberOfInstances.each do |currentNumberOfInstances|
+	maximumNumberOfTunnels.each do |currentNumberOfTunnels|
+	  simulate "multiserver", numberOfSupportedParallelTunnels: currentNumberOfTunnels,
+	    maxInstanceNumber: maximumNumberOfInstances,
+	    duration: 7.day + 1.hour,
+	    transientPhaseDuration: 1.hour
+	end
+end
