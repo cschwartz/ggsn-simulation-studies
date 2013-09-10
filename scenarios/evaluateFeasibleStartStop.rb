47644beda46997c8c80e808d39890482f9ff6f2b
Florian Metzger <florian.metzger@univie.ac.at>
2013-09-10 10:01:11 +0200

    recreated startstop scenario

--- scenarios/evaluateFeasibleStartStop.rb
+++ scenarios/evaluateFeasibleStartStop.rb
@@ -1,17 +1,23 @@
 repetitions 10
-maximumNumberOfTunnels = (100..600).step(100)
-maximumNumberOfInstances = (10...100).step(10)
-startstop_time = [60,120,180,240,300]
 
-startstop_time.each do |currentStartStopTime|
-	maximumNumberOfInstances.each do |currentNumberOfInstances|
-		maximumNumberOfTunnels.each do |currentNumberOfTunnels|
-		  simulate "multiserver", numberOfSupportedParallelTunnels: currentNumberOfTunnels,
+maximumNumberOfInstances = (10..100).step(10)
+startstopTime = [60,120,180,240,300]
+
+maximumNumberOfTunnels = [(100..700).step(100), (100..400).step(100), (50..300).step(50), (50..200).step(50), (50..200).step(50), (50..150).step(50), (25..100).step(25), (25..100).step(25), (20..80).step(20), (10..70).step(10)]
+
+
+
+i = 0
+maximumNumberOfInstances.each do |currentNumberOfInstances|
+	maximumNumberOfTunnels[i].each do |currentNumberOfTunnels|
+		startstopTime.each do |currentStartStopTime|
+			simulate "multiserver", numberOfSupportedParallelTunnels: currentNumberOfTunnels,
 		    maxInstanceNumber: currentNumberOfInstances,
 		    duration: 7.day + 1.hour,
 		    transientPhaseDuration: 1.hour,
-		    startup_time: currentStartStopTime,
-		    shutdown_time: currentStartStopTime
+		    startupTime: currentStartStopTime,
+		    shutdownTime: currentStartStopTime
 		end
 	end
+	i += 1
 end
