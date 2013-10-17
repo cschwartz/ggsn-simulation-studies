779184bc9bcf12f77e851a4ed7c106cb0cbb4d35
Florian Metzger <florian.metzger@univie.ac.at>
2013-10-17 14:19:23 +0200

    that approach should work more reliably

--- scenarios/evaluateFeasibleMultiserver.rb
+++ scenarios/evaluateFeasibleMultiserver.rb
@@ -1,14 +1,14 @@
 repetitions 10
 maximumNumberOfTunnels = (0..1000).step(50)
 #maximumNumberOfInstances = (10...100).step(10)
-maximumNumberOfInstances = [[100]]
+currentNumberOfInstances = 100
 
 
-maximumNumberOfInstances.each do |currentNumberOfInstances|
+# maximumNumberOfInstances.each do |currentNumberOfInstances|
 	maximumNumberOfTunnels.each do |currentNumberOfTunnels|
 	  simulate "multiserver", numberOfSupportedParallelTunnels: currentNumberOfTunnels,
 	    maxInstanceNumber: currentNumberOfInstances,
 	    duration: 7.day + 1.hour,
 	    transientPhaseDuration: 1.hour
 	end
-end
+# end
